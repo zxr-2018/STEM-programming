2c7904688025d901a5f91b4a7353bc31b2f1d82d
zhu siri <42911563+zxr-2018@users.noreply.github.com>
2018-11-24 21:55:08 +0800

    Update 芝麻开门程序数字版.py
    03V1 芝麻开门程序数字版

--- 芝麻开门程序数字版.py
+++ 芝麻开门程序数字版.py
@@ -2,6 +2,6 @@
 password ='060429'
 number = raw_input("Please enter your password: ")
 if number == password:
-    print("You have one billion!")
+    print("You have one billion.")
 else:
     print("The password is wrong.")
